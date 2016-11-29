d98f7be99e1af17d8bfe49632a008c6312779ce1
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-11-29 16:05:17 -0500

    This will allow you to manage notifications in the program incase they are removed from the sidebar.

--- Spillway/Views/NotificationView.xaml.cs
+++ Spillway/Views/NotificationView.xaml.cs
@@ -20,9 +20,40 @@ namespace Spillway.Views
     /// </summary>
     public partial class NotificationView : UserControl
     {
+        public static readonly DependencyProperty DblClickCommandProperty = DependencyProperty.Register("DblClickCommand", typeof(ICommand), typeof(NotificationView), new PropertyMetadata(DblClickCommandChanged));
+        public ICommand DblClickCommand
+        {
+            get { return (ICommand)GetValue(DblClickCommandProperty); }
+            set { SetValue(DblClickCommandProperty, value); }
+        }
+        private static void DblClickCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = d as NotificationView;
+        }
+
+
+        public static readonly DependencyProperty DblClickParamProperty = DependencyProperty.Register("DblClickParam", typeof(object), typeof(NotificationView), new PropertyMetadata(DblClickParamChanged));
+        public object DblClickParam
+        {
+            get { return (object)GetValue(DblClickParamProperty); }
+            set { SetValue(DblClickParamProperty, value); }
+        }
+        private static void DblClickParamChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = d as NotificationView;
+        }
+
+
+
         public NotificationView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Note(Mattew): you could techinically validate this in order to make sure the webaddress is really valid but for right now this is fine. 
+            DblClickCommand?.Execute(DblClickParam);
         }
     }
 }
