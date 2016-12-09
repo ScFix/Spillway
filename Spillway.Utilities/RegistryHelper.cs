2ba8de894d54018b8b274d6eed0c4803c1989602
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-12-09 00:43:00 -0500

    Options, along with the ability to set spillway to autostart upon starting computer

---
+++ Spillway.Utilities/RegistryHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spillway.Utilities
+{
+	public static class RegistryHelper
+	{
+		//registryKey.SetValue("ApplicationName", Application.ExecutablePath);
+		public static void RegisterAppToStartup(bool setAppToStartUp, string appName)
+		{
+			RegistryKey registyKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN", true);
+			if (setAppToStartUp)
+			{
+				registyKey.SetValue(appName, System.Reflection.Assembly.GetExecutingAssembly().Location);
+			}
+			else
+			{
+				registyKey.DeleteValue(appName);
+			}
+		}
+		public static bool IsAppRegisteredToStartUp(string appName)
+		{
+			RegistryKey registyKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN", true);
+			return registyKey.GetValue(appName) != null;
+		}
+	}
+}
--- Spillway/ViewModels/OptionsViewModel.cs
+++ Spillway/ViewModels/OptionsViewModel.cs
@@ -1,18 +1,53 @@
 ﻿using Spillway.Interfaces;
+using Spillway.Utilities;
 using System;
 
 namespace Spillway.ViewModels
 {
-    public class OptionsViewModel : ViewModelBase, ISection
-    {
-        public override string ToString()
-        {
-            return "Options";
-        }
+	public class OptionsViewModel : ViewModelBase, ISection
+	{
 
-        public String SectionName
-        {
-            get { return ToString(); }
-        }
-    }
+		public OptionsViewModel()
+		{
+			// TODO(Matthew): Get rid of the static name of spillway or reduce the occurance of them but they do not need to be hard coded in
+			_IsAutoStart = RegistryHelper.IsAppRegisteredToStartUp("Spillway");
+		}
+
+		#region IsAutoStart
+		protected bool _IsAutoStart;
+		public bool IsAutoStart
+		{
+			get
+			{
+				return _IsAutoStart;
+			}
+			set
+			{
+				if (value != _IsAutoStart)
+				{
+					_IsAutoStart = value;
+					SetAutoStart(_IsAutoStart);
+					OnPropertyChanged("IsAutoStart");
+				}
+			}
+		}
+
+		#endregion //IsAutoStart
+
+		public override string ToString()
+		{
+			return "Options";
+		}
+
+		public String SectionName
+		{
+			get { return ToString(); }
+		}
+
+		private void SetAutoStart(bool isAutoStart)
+		{
+			RegistryHelper.RegisterAppToStartup(isAutoStart, "Spillway");
+		}
+
+	}
 }
